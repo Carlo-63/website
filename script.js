38ea17aa70af263d51476c7cda1463e4467a5b69
Carlo-63 <76780141+Carlo-63@users.noreply.github.com>
2023-12-22 10:10:10 +0100

    Main Update #1

---
+++ script.js
@@ -0,0 +1,11 @@
+const showModal = () => {
+    const modal = document.querySelector('.social-modal');
+
+    modal.style.display = "flex";
+}
+
+const hideModal = () => {
+    const modal = document.querySelector('.social-modal');
+    
+    modal.style.display = "none";
+}
